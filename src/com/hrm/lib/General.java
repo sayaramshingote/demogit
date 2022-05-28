d1aaf1a833c7a104c56540fb9d8a85c495337c06
sayaram <sayaram.shingote91@gmail.com>
2022-05-28 09:25:04 +0530

    My demo script messanger

---
+++ src/com/hrm/lib/General.java
@@ -0,0 +1,7 @@
+package com.hrm.lib;
+
+public class General {
+	//std Rules- To provide reusable fun/method related to whole applin
+
+
+}
---
+++ src/com/hrm/lib/Global.java
@@ -0,0 +1,6 @@
+package com.hrm.lib;
+
+public class Global {
+	//std Rules- To provide test data & object related to whole applin
+
+}
